b68a421c1617fdd388985a52d24cc501455c1e7a
zeeshan <itexpert120@gmail.com>
2021-01-27 20:27:52 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := device/samsung/a33g
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/rr_a33g.mk \
-	$(LOCAL_DIR)/pa_a33g.mk \
 	$(LOCAL_DIR)/lineage_a33g.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a33g-eng \
+    lineage_a33g-userdebug \
+    lineage_a33g-user
